9e432b16e93460c49da8ed7ff628f5a1211448e1
gedasga <gedas.gardauskas@gmail.com>
2020-05-30 13:17:10 +0300

    feat: add links to user icons, create fuctions that call endpoints

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,24 @@
-// TODO: Add integration with fake _DATA database
+import {
+  _getUsers,
+  _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer,
+} from './_DATA.js'
+
+export function getInitialData () {
+  return Promise.all([
+    _getUsers(),
+    _getQuestions(),
+  ]).then(([users, questions]) => ({
+    users,
+    questions,
+  }))
+}
+
+export function saveQuestion(question) {
+  return _saveQuestion(question)
+}
+
+export function saveQuestionAnswer(answer) {
+  return _saveQuestionAnswer(answer)
+}
